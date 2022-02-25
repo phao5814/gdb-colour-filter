979c535ee127abe71eddd8e90ca94b6146565903
phao5814 <patrick.hao95@gmail.com>
2022-02-25 15:28:06 +1100

    Converted script to Python 2

--- colour_filter.py
+++ colour_filter.py
@@ -2,7 +2,8 @@
 #   encoding: utf8
 #   file: colour_filter.py
 
-from typing import Iterator, Text
+from __future__ import absolute_import
+# from typing import Iterator, Text
 
 from gdb import parameter as get_parameter
 from gdb import Frame, frame_filters, execute
@@ -10,7 +11,7 @@ from gdb.FrameDecorator import FrameDecorator
 
 
 class FrameColorizer(FrameDecorator):
-    """FrameColorizer repeats all actions to get all common frame attribute and
+    u"""FrameColorizer repeats all actions to get all common frame attribute and
     then spices a bit output with colours. Format of output string is following.
 
     #<depth> <address> in <function> (<frame_args>) at <filename>[:<line>]
@@ -18,32 +19,34 @@ class FrameColorizer(FrameDecorator):
     Notes: There is not special support Frame.elided() property.
     """
 
-    def __init__(self, *args, depth=0, **kwargs):
+    def __init__(self, *args, **kwargs):
+        if 'depth' in kwargs: depth = kwargs['depth']; del kwargs['depth']
+        else: depth = 0
         super(FrameColorizer, self).__init__(*args, **kwargs)
 
         self._depth = depth
         self.frame = super(FrameColorizer, self).inferior_frame()
 
     def __str__(self):
-        is_print_address = get_parameter('print address')
+        is_print_address = get_parameter(u'print address')
 
         part1 = self.depth()
-        part2 = self.function() + ' \033[1;37m(' + self.frame_args() + ')\033[0m'
+        part2 = self.function() + u' \033[1;37m(' + self.frame_args() + u')\033[0m'
         part3 = self.filename() + self.line()
 
         if is_print_address:
-            part1 += '  ' + self.address() + ' in '
+            part1 += u'  ' + self.address() + u' in '
         else:
-            part1 += ' '
+            part1 += u' '
 
-        parts = part1 + part2 + ' at ' + part3
+        parts = part1 + part2 + u' at ' + part3
 
         screen_width = self.get_screen_width()
         if screen_width is not None and len(parts) > screen_width:
             shift_width = int(self.length(part1)) - 1
             shift_width -= 3 * int(is_print_address)  # compensate ' in ' part
-            value = part1 + part2 + '\n'
-            value += ' ' * shift_width + ' at ' + part3
+            value = part1 + part2 + u'\n'
+            value += u' ' * shift_width + u' at ' + part3
         else:
             value = parts
 
@@ -51,14 +54,14 @@ class FrameColorizer(FrameDecorator):
 
     def address(self):
         address = super(FrameColorizer, self).address()
-        return '\033[1;30m0x%016x\033[0m' % address
+        return u'\033[1;30m0x%016x\033[0m' % address
 
     def depth(self):
-        return '\033[1;37m#%-3d\033[0m' % self._depth
+        return u'\033[1;37m#%-3d\033[0m' % self._depth
 
     def filename(self):
         filename = super(FrameColorizer, self).filename()
-        return '\033[0;36m%s\033[0m' % filename
+        return u'\033[0;36m%s\033[0m' % filename
 
     def frame_args(self):
         try:
@@ -72,7 +75,7 @@ class FrameColorizer(FrameDecorator):
             block = block.superblock
 
         if block is None:
-            return ''
+            return u''
 
         args = []
 
@@ -80,10 +83,10 @@ class FrameColorizer(FrameDecorator):
             if not sym.is_argument:
                 continue;
             val = sym.value(self.frame)
-            arg = '%s=%s' % (sym, val) if str(val) else str(sym)
+            arg = u'%s=%s' % (sym, val) if unicode(val) else unicode(sym)
             args.append(arg)
 
-        return ', '.join(args)
+        return u', '.join(args)
 
     def function(self):
         func = super(FrameColorizer, self).function()
@@ -94,15 +97,15 @@ class FrameColorizer(FrameDecorator):
             # Here we have something like
             # > raise + 272 in section .text of /usr/lib/libc.so.6
             # XXX: gdb.find_pc_line
-            symbol = gdb.execute('info symbol 0x%016x' % func, False, True)
+            symbol = gdb.execute(u'info symbol 0x%016x' % func, False, True)
 
             # But here we truncate layout in binary
             # > raise + 272
-            name = symbol[:symbol.find('in section')].strip()
+            name = symbol[:symbol.find(u'in section')].strip()
 
             # Check if we in format
             # > smthing + offset
-            parts = name.rsplit(' ', 1)
+            parts = name.rsplit(u' ', 1)
             # > raise
             if len(parts) == 1:
                 return name
@@ -112,34 +115,34 @@ class FrameColorizer(FrameDecorator):
             except ValueError:
                 return name
 
-            return '\033[1;34m' + parts[0] + ' ' + offset + '\033[0m'
+            return u'\033[1;34m' + parts[0] + u' ' + offset + u'\033[0m'
         else:
-            return '\033[1;34m' + func + '\033[0m'
+            return u'\033[1;34m' + func + u'\033[0m'
 
     def get_screen_width(self):
-        """Get screen width from GDB. Source format is following
+        u"""Get screen width from GDB. Source format is following
         """
-        return get_parameter('width')
+        return get_parameter(u'width')
 
     def line(self):
         value = super(FrameColorizer, self).line()
-        return '\033[0;35m:%d\033[0m' % value if value else ''
+        return u'\033[0;35m:%d\033[0m' % value if value else u''
 
     @staticmethod
     def length(colored_string):
-        """This function calculates length of string with terminal control
+        u"""This function calculates length of string with terminal control
         sequences.
         """
         start = 0
         term_seq_len = 0
 
         while True:
-            begin = colored_string.find('\033', start)
+            begin = colored_string.find(u'\033', start)
 
             if begin == -1:
                 break
 
-            end = colored_string.find('m', begin)
+            end = colored_string.find(u'm', begin)
 
             if end == -1:
                 end = len(s)
@@ -150,31 +153,31 @@ class FrameColorizer(FrameDecorator):
         return len(colored_string) - term_seq_len
 
 
-class FilterProxy:
-    """FilterProxy class keep ensures that frame iterator will be comsumed
+class FilterProxy(object):
+    u"""FilterProxy class keep ensures that frame iterator will be comsumed
     properly on the first and the sole call.
     """
 
-    def __init__(self, frames: Iterator[Frame]):
+    def __init__(self, frames):
         self.frames = (FrameColorizer(frame, depth=ix)
                        for ix, frame in enumerate(frames))
 
     def __iter__(self):
         return self
 
-    def __next__(self):
+    def next(self):
         self.unroll_stack()
         raise StopIteration
 
     def unroll_stack(self):
-        output = (str(frame) for frame in self.frames)
-        print('\n'.join(output))
+        output = (unicode(frame) for frame in self.frames)
+        print u'\n'.join(output)
 
 
-class ColourFilter:
+class ColourFilter(object):
 
-    def __init__(self, name='backtrace-filter', priority=0, enabled=True):
-        """Frame filter with the lower priority that consumes every frame and
+    def __init__(self, name=u'backtrace-filter', priority=0, enabled=True):
+        u"""Frame filter with the lower priority that consumes every frame and
         colouring output.
 
         :param name: The name of the filter that GDB will display.
@@ -190,7 +193,7 @@ class ColourFilter:
         # dictionary.
         frame_filters[self.name] = self
 
-    def filter(self, iters: Iterator[Frame]) -> Iterator[Frame]:
+    def filter(self, iters):
         return FilterProxy(iters)
 
 
